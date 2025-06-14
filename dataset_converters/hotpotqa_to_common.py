df8105615d62eb802d60320b3d12342bd850b8dd
Ivan <halkimic@gmail.com>
2025-06-14 11:06:59 +0300

    redesign converter

--- dataset_converters/hotpotqa_to_common.py
+++ dataset_converters/hotpotqa_to_common.py
@@ -1,19 +1,44 @@
 import argparse
 import json
+from pathlib import Path
+import os
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset_json_path', type=str)
-    parser.add_argument('--output_path', type=str)
+    parser.add_argument('--num_questions', type=str)
+    parser.add_argument('--output_folder_path', type=str)
     args = parser.parse_args()
     input_path = args.dataset_json_path
-    output_path = args.output_path
+    output_folder_path = args.output_folder_path
 
     with open(input_path, 'r') as input_file:
         corpus = json.load(input_file)
-    keys = list(corpus.keys())
-    retrieval_corpus = [{'idx': i, 'passage': key + '\n' + ''.join(corpus[key])} for i, key in enumerate(keys)]
-
-    with open(output_path, 'w') as output_file:
-        corpus = json.dump(retrieval_corpus, output_file)
+    
+    num_questions = int(args.num_questions)
+    retrieval_corpus = []
+    questions = []
+    index = 0
+    for sample in corpus:
+        if len(questions) == num_questions:
+            break
+        question = sample['question']
+        questions.append(question)
+        for context_text in sample['context']:
+            cur_context = ' '.join(context_text[1])
+            reorganized_sample = {"idx": index, "passage": cur_context}
+            retrieval_corpus.append(reorganized_sample)
+            index += 1
+    
+    print(f"{len(questions)} questions")
+    print(f"{len(retrieval_corpus)} paragraphs")
+    filename = Path(input_path).stem
+    set_name = '_'.join(filename.split('_')[:-1])
+    num_questions = 'all' if num_questions == len(corpus) else num_questions 
+    output_file_path = os.path.join(Path(output_folder_path), Path(f"{set_name}_corpus_{num_questions}.json"))
+    with open(output_file_path, 'w') as output_file:
+        corpus = json.dump(retrieval_corpus, output_file, indent=4)
+    output_file_path = os.path.join(Path(output_folder_path), Path(f"{set_name}_questions_{num_questions}.json"))
+    with open(output_file_path, 'w') as output_file:
+        corpus = json.dump(retrieval_corpus, output_file, indent=4)
