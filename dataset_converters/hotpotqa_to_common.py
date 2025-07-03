9001a08fb968774fb3a568c0ead4f54bb15cb3f0
Ivan <halkimic@gmail.com>
2025-07-03 19:27:13 +0300

    changes in converters

--- dataset_converters/hotpotqa_to_common.py
+++ dataset_converters/hotpotqa_to_common.py
@@ -28,11 +28,11 @@ if __name__ == '__main__':
         texts_index_list = []
         for context_text in sample['context']:
             cur_context = ' '.join(context_text[1])
-            reorganized_sample = {"idx": index, "passage": cur_context}
+            reorganized_sample = {"id": sample["_id"], "index": index, "passage": cur_context}
             retrieval_corpus.append(reorganized_sample)
             texts_index_list.append(index)
             index += 1
-        question_full_info = {"_id": sample["_id"], "question": question, "ref_paragraphs": texts_index_list}
+        question_full_info = {"id": sample["_id"], "question": question, "ref_paragraphs": texts_index_list}
         questions.append(question_full_info)
     
     print(f"{len(questions)} questions")
