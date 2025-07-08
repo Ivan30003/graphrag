14bd5cc6bc3e2729c61377b0f0f66c34a754f208
Ivan <halkimic@gmail.com>
2025-07-08 09:46:22 +0300

    class expand triple

--- class_triples_entities.py
+++ class_triples_entities.py
@@ -5,6 +5,8 @@ class Triple:
         self.linkage = linkage_entity
         self.second_entity = second_entity
         self.text_idx = text_idx
+        self.content_list = [first_entity, linkage_entity, second_entity]
+        self.table_represent = self.content_list + [self.text_idx]
 
     def __eq__(self, other):
         return self.first_entity == other.first_entity and self.linkage == other.linkage and \
@@ -14,38 +16,20 @@ class Triple:
         return hash(tuple([self.first_entity, self.linkage, self.second_entity]))
 
 
-# class Triple:
-#     def __init__(self, first_entity, linkage_entity, second_entity):
-#         self.length = 0
-#         self.entities = []
-#         if first_entity and type(first_entity) == str:
-#             self.first_entity = first_entity
-#             self.length += 1
-#             self.entities.append(first_entity)
-#         else:
-#             self.first_entity = "_"
-        
-#         if linkage_entity and type(linkage_entity) == str:
-#             self.linkage_entity = linkage_entity
-#             self.length += 1
-#         else:
-#             self.linkage_entity = "_"
-        
-#         if second_entity and type(second_entity) == str:
-#             self.second_entity = second_entity
-#             self.length += 1
-#             self.entities.append(second_entity)
-#         else:
-#             self.second_entity = "_"
+class ExpandTriple:
+    def __init__(self, content_list, text_idx):
+        self.length = 0
+        if type(content_list) == str:
+            self.content_list = [content_list]
+        else:
+            self.content_list = content_list
 
-#     def __repr__(self) -> str:
-#         return f"{self.first_entity} - {self.linkage_entity} - {self.second_entity}"
+        self.text_idx = text_idx
+        self.table_represent = content_list + [self.text_idx]
 
-#     def __len__(self) -> int:
-#         return 
+    def __eq__(self, other):
+        return self.first_entity == other.first_entity and self.linkage == other.linkage and \
+        self.second_entity == other.second_entity
 
-#     # def check_integrity(self):
-#     #     if self.first_entity and self.linkage_entity and self.second_entity:
-#     #         return True
-#     #     else:
-#     #         return False
+    def __hash__(self):
+        return hash(tuple(self.content_list))
