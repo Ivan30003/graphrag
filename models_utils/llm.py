f92c16f0d266ea97293e595913e0484551edaba5
Ivan <halkimic@gmail.com>
2026-03-25 12:53:33 +0300

    refactor llm part

--- models_utils/llm.py
+++ models_utils/llm.py
@@ -40,6 +40,21 @@ def init_langchain_model(llm: str, model_name: str, temperature: float = 0.0,
         return model
 
 
+class CustomLLM:
+    def __init__(self) -> None:
+        pass
+
+    def is_repeat(self, text: str):
+        phrases = text.split('\n')
+        if len(phrases) < MIN_JSON_LENGTH:
+            return False
+        for i in range(len(phrases)-2):
+            if phrases[i] == phrases[i+1] and phrases[i+1] == phrases[i+2]:
+                return True
+
+        return False
+
+
 class LLM_Phi_35:
     def __init__(self, model_name_or_path) -> None:
         model = AutoModelForCausalLM.from_pretrained(
@@ -93,8 +108,7 @@ class LLM_Qwen_3:
             torch_dtype="auto",
             device_map="auto"
         )
-        self.max_attempts = 2
-
+        # self.model.bfloat16()
         self.messages = [
             {"role": "system", "content": "You are a helpful AI assistant."},
             {"role": "user", "content": ""},
@@ -102,22 +116,16 @@ class LLM_Qwen_3:
             {"role": "user", "content": ""},
         ]
 
-    def is_repeat(self, text: str):
-        phrases = text.split('\n')
-        if len(phrases) < MIN_JSON_LENGTH:
-            return False
-        for i in range(len(phrases)-2):
-            if phrases[i] == phrases[i+1] and phrases[i+1] == phrases[i+2]:
-                return True
-
-        return False
-
-
     def invoke(self, openie_messages: ChatPromptTemplate, temperature=0.0, max_tokens=4096, task='ner'):
         messages_length = len(openie_messages)
         assert messages_length in [2, 4]
         print(f"{'*'*70}\n{openie_messages}\n\n")
         # messages_template = self.messages[:messages_length]
+
+        do_sample = False
+        if temperature > 0.0:
+            do_sample = True
+
         for ind in range(messages_length):
             self.messages[ind]['content'] = openie_messages[ind].content
 
@@ -129,25 +137,23 @@ class LLM_Qwen_3:
         )
         model_inputs = self.tokenizer([text], return_tensors="pt").to(self.model.device)
 
-        for i in range(self.max_attempts):
-            # do_sample = False
-            # if temperature > 0.0:
-            #     do_sample = True
-            generated_ids = self.model.generate(
-                **model_inputs,
-                max_new_tokens=max_tokens
-            )
+        generated_ids = self.model.generate(
+            **model_inputs,
+            max_new_tokens=max_tokens,
+            do_sample=do_sample,
+            temperature=temperature
+        )
 
-            output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist()
+        output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist()
 
-            content = self.tokenizer.decode(output_ids, skip_special_tokens=True).strip("\n")
-            if self.is_repeat(content):
-                print(f"ANSWER:\n{[content]}")
-                print(f"REPEAT, trying again")
-            else:
-                break
+        content = self.tokenizer.decode(output_ids, skip_special_tokens=True).strip("\n")
+            # if self.is_repeat(content):
+            #     print(f"ANSWER:\n{[content]}")
+            #     print(f"REPEAT, trying again")
+            # else:
+            #     break
 
-        print(f"ANSWER:\n{[content]}\n\n\n")
+        print(f"\nANSWER:\n{[content]}\n\n\n")
         return [ None, None, None, None, {'content': content}]
 
 
@@ -167,18 +173,10 @@ class LLM_Granite_33:
             {"role": "user", "content": ""},
         ]
 
-        # self.pipe = pipeline(
-        #     "text-generation",
-        #     model=model,
-        #     device="cuda",
-        #     tokenizer=tokenizer,
-        # )
-
     def invoke(self, openie_messages: ChatPromptTemplate, temperature=0.0, max_tokens=3096, task='ner'):
         messages_length = len(openie_messages)
         assert messages_length in [2, 4]
         print(f"{'*'*70}\n{openie_messages}\n\n")
-        # messages_template = self.messages[:messages_length]
         for ind in range(messages_length):
             self.messages[ind]['content'] = openie_messages[ind].content
 
@@ -189,7 +187,6 @@ class LLM_Granite_33:
             enable_thinking=False, # Switches between thinking and non-thinking modes. Default is True.
             return_dict=True
         ).to(self.model.device)
-        # model_inputs = self.tokenizer([text], return_tensors="pt").to(self.model.device)
         do_sample = False
         if temperature > 0.0:
             do_sample = True
@@ -199,17 +196,8 @@ class LLM_Granite_33:
             do_sample=do_sample,
             temperature=temperature
         )
-        # output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
-
-        # try:
-        #     # rindex finding 151668 (</think>)
-        #     index = len(output_ids) - output_ids[::-1].index(151668)
-        # except ValueError:
-        #     index = 0
-
-        # content = self.tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
         content = self.tokenizer.decode(generated_ids[0, model_inputs["input_ids"].shape[1]:], skip_special_tokens=True)
-        # print(f"ANSWER:\n{output[0]['generated_text'][4]['content']}")
+        
         print(f"{'*'*70}\n{content}\n\n")
         return [ None, None, None, None, {'content': content}]
 
